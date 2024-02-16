e52d78c635a83c384dea51e2cdf2dc5fa2b200f2
Degubi <gergonoorbi@gmail.com>
2024-02-16 11:38:38 +0100

    Upload Day20 2015

---
+++ 2015/Day20.java
@@ -0,0 +1,36 @@
+import java.nio.file.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class Day20 {
+
+
+    public static void main(String[] args) throws Exception {
+        var presents = Integer.parseInt(Files.readString(Path.of("Day20.txt")));
+        var part1Result = IntStream.range(1, Integer.MAX_VALUE)
+                                   .filter(k -> calculatePresentAmount(k, 10, d -> true) >= presents)
+                                   .findFirst()
+                                   .orElseThrow();
+
+        var part2Result = IntStream.range(1, Integer.MAX_VALUE)
+                                   .filter(k -> calculatePresentAmount(k, 11, d -> d <= 50) >= presents)
+                                   .findFirst()
+                                   .orElseThrow();
+
+        System.out.println("Result 1: " + part1Result + ", result 2: " + part2Result);
+    }
+
+    static int calculatePresentAmount(int houseNumber, int presentsPerDelivery, IntPredicate divisorPredicate) {
+        return IntStream.rangeClosed(1, (int) Math.sqrt(houseNumber))
+                        .filter(k -> houseNumber % k == 0)
+                        .flatMap(k -> getDivisorPairs(k, houseNumber))
+                        .filter(k -> divisorPredicate.test(houseNumber / k))
+                        .sum() * presentsPerDelivery;
+    }
+
+    static IntStream getDivisorPairs(int divisor, int value) {
+        var other = value / divisor;
+
+        return divisor == other ? IntStream.of(divisor) : IntStream.of(divisor, other);
+    }
+}
