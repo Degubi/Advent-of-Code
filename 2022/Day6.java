458910d26c5f8e28a87e92d73ab5dc71103f6876
Degubi <gergonoorbi@gmail.com>
2024-02-28 09:22:11 +0100

    Upload Day6 2022

---
+++ 2022/Day6.java
@@ -0,0 +1,21 @@
+import java.nio.file.*;
+import java.util.stream.*;
+
+public class Day6 {
+
+
+    public static void main(String[] args) throws Exception {
+        var input = Files.readString(Path.of("Day6.txt"));
+        var part1Result = findMarkerPosition(input, 4);
+        var part2Result = findMarkerPosition(input, 14);
+
+        System.out.println("Result 1: " + part1Result + ", result 2: " + part2Result);
+    }
+
+    static int findMarkerPosition(String text, int distinctCharCount) {
+        return IntStream.range(0, text.length() - distinctCharCount)
+                        .filter(i -> text.substring(i, i + distinctCharCount).chars().distinct().count() == distinctCharCount)
+                        .findFirst()
+                        .orElse(-distinctCharCount - 1) + distinctCharCount;
+    }
+}
