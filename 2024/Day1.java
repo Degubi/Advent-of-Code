8efed4f23eccf29a18a0763d9687bcaa93bc8542
Degubi <gergonoorbi@gmail.com>
2024-12-01 08:09:31 +0100

    Upload Day 1 of 2024

---
+++ 2024/Day1.java
@@ -0,0 +1,37 @@
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        var input = Files.lines(Path.of("input.txt"))
+                         .map(k -> k.split("   ", 2))
+                         .toArray(String[][]::new);
+
+        var firstColumnNumbers = parseInputColumn(input, 0);
+        var secondColumnNumbers = parseInputColumn(input, 1);
+
+        var secondColumnNumberFrequencies = Arrays.stream(secondColumnNumbers)
+                                                  .boxed()
+                                                  .collect(Collectors.groupingBy(k -> k, Collectors.counting()));
+
+        var part1Result = IntStream.range(0, input.length)
+                                   .map(i -> Math.abs(firstColumnNumbers[i]- secondColumnNumbers[i]))
+                                   .sum();
+
+        var part2Result = Arrays.stream(firstColumnNumbers)
+                                .map(k -> k * secondColumnNumberFrequencies.getOrDefault(k, 0L).intValue())
+                                .sum();
+
+        System.out.println("Part 1 Result: " + part1Result);
+        System.out.println("Part 2 Result: " + part2Result);
+    }
+
+    static int[] parseInputColumn(String[][] input, int columnI) {
+        return IntStream.range(0, input.length)
+                        .map(i -> Integer.parseInt(input[i][columnI]))
+                        .sorted()
+                        .toArray();
+    }
+}
