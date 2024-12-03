a9d55eda88f7baf37992a4ae7b2675ae5e83f638
Degubi <gergonoorbi@gmail.com>
2024-12-03 18:19:19 +0100

    Upload Day2 2024

---
+++ 2024/Day2.java
@@ -0,0 +1,40 @@
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        var values = Files.lines(Path.of("input.txt"))
+                          .map(k -> Arrays.stream(k.split(" ")).mapToInt(Integer::parseInt).toArray())
+                          .toArray(int[][]::new);
+
+        var part1Result = Arrays.stream(values)
+                                .filter(Main::isReportValid)
+                                .count();
+
+        var part2Result = Arrays.stream(values)
+                                .filter(k -> isReportValid(k) || IntStream.range(0, k.length).anyMatch(r -> isReportValid(removeElementAtIndex(k, r))))
+                                .count();
+
+        System.out.println("Result 1: " + part1Result + ", result 2: " + part2Result);
+    }
+
+    static boolean isReportValid(int[] values) {
+        return IntStream.range(0, values.length -1)
+                        .reduce((int) Math.signum(values[0] - values[1]), (lastSign, i) -> {
+                            var diff = values[i] - values[i + 1];
+                            var currentSign = (int) Math.signum(diff);
+                            var absDiff = Math.abs(diff);
+
+                            return lastSign != currentSign || absDiff < 1 || absDiff > 3 ? Integer.MIN_VALUE : currentSign;
+                        }) != Integer.MIN_VALUE;
+    }
+
+    static int[] removeElementAtIndex(int[] values, int index) {
+        return IntStream.range(0, values.length)
+                        .filter(i -> i != index)
+                        .map(i -> values[i])
+                        .toArray();
+    }
+}
