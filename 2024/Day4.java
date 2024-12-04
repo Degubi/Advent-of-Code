3a86a5ba072c1511f3e1128b9322e554718c1735
Degubi <gergonoorbi@gmail.com>
2024-12-04 17:46:43 +0100

    Update Day4.java

--- 2024/Day4.java
+++ 2024/Day4.java
@@ -14,46 +14,43 @@ public class Main {
 
         var part1Result = IntStream.range(0, rowCount)
                                    .map(rowI -> IntStream.range(0, columnCount)
-                                                         .map(columnI -> countXMASAtPosition(rowI, columnI, input))
+                                                         .map(colI -> countXMASAtPosition(rowI, colI, input))
                                                          .sum())
                                    .sum();
 
         var part2Result = IntStream.range(0, rowCount)
                                    .map(rowI -> IntStream.range(0, columnCount)
-                                                         .map(columnI -> countCrossMASAtPosition(rowI, columnI, input))
+                                                         .map(colI -> countCrossMASAtPosition(rowI, colI, input))
                                                          .sum())
                                    .sum();
 
         System.out.println("Result 1: " + part1Result + ", result 2: " + part2Result);
     }
 
-    static int countXMASAtPosition(int rowI, int columnI, char[][] input) {
-        return getCharAt(rowI, columnI, input) != 'X' ? 0 :
-                  (getCharAt(rowI, columnI + 1, input) == 'M' && getCharAt(rowI, columnI + 2, input) == 'A' && getCharAt(rowI, columnI + 3, input) == 'S' ? 1 : 0) +
-                  (getCharAt(rowI, columnI - 1, input) == 'M' && getCharAt(rowI, columnI - 2, input) == 'A' && getCharAt(rowI, columnI - 3, input) == 'S' ? 1 : 0) +
+    static int countXMASAtPosition(int rowI, int colI, char[][] input) {
+        return getCharAt(rowI, colI, input) != 'X' ? 0 :
+                  (getCharAt(rowI, colI + 1, input) == 'M' && getCharAt(rowI, colI + 2, input) == 'A' && getCharAt(rowI, colI + 3, input) == 'S' ? 1 : 0) +
+                  (getCharAt(rowI, colI - 1, input) == 'M' && getCharAt(rowI, colI - 2, input) == 'A' && getCharAt(rowI, colI - 3, input) == 'S' ? 1 : 0) +
 
-                  (getCharAt(rowI + 1, columnI, input) == 'M' && getCharAt(rowI + 2, columnI, input) == 'A' && getCharAt(rowI + 3, columnI, input) == 'S' ? 1 : 0) +
-                  (getCharAt(rowI - 1, columnI, input) == 'M' && getCharAt(rowI - 2, columnI, input) == 'A' && getCharAt(rowI - 3, columnI, input) == 'S' ? 1 : 0) +
+                  (getCharAt(rowI + 1, colI, input) == 'M' && getCharAt(rowI + 2, colI, input) == 'A' && getCharAt(rowI + 3, colI, input) == 'S' ? 1 : 0) +
+                  (getCharAt(rowI - 1, colI, input) == 'M' && getCharAt(rowI - 2, colI, input) == 'A' && getCharAt(rowI - 3, colI, input) == 'S' ? 1 : 0) +
 
-                  (getCharAt(rowI + 1, columnI + 1, input) == 'M' && getCharAt(rowI + 2, columnI + 2, input) == 'A' && getCharAt(rowI + 3, columnI + 3, input) == 'S' ? 1 : 0) +
-                  (getCharAt(rowI + 1, columnI - 1, input) == 'M' && getCharAt(rowI + 2, columnI - 2, input) == 'A' && getCharAt(rowI + 3, columnI - 3, input) == 'S' ? 1 : 0) +
-                  (getCharAt(rowI - 1, columnI + 1, input) == 'M' && getCharAt(rowI - 2, columnI + 2, input) == 'A' && getCharAt(rowI - 3, columnI + 3, input) == 'S' ? 1 : 0) +
-                  (getCharAt(rowI - 1, columnI - 1, input) == 'M' && getCharAt(rowI - 2, columnI - 2, input) == 'A' && getCharAt(rowI - 3, columnI - 3, input) == 'S' ? 1 : 0);
+                  (getCharAt(rowI + 1, colI + 1, input) == 'M' && getCharAt(rowI + 2, colI + 2, input) == 'A' && getCharAt(rowI + 3, colI + 3, input) == 'S' ? 1 : 0) +
+                  (getCharAt(rowI + 1, colI - 1, input) == 'M' && getCharAt(rowI + 2, colI - 2, input) == 'A' && getCharAt(rowI + 3, colI - 3, input) == 'S' ? 1 : 0) +
+                  (getCharAt(rowI - 1, colI + 1, input) == 'M' && getCharAt(rowI - 2, colI + 2, input) == 'A' && getCharAt(rowI - 3, colI + 3, input) == 'S' ? 1 : 0) +
+                  (getCharAt(rowI - 1, colI - 1, input) == 'M' && getCharAt(rowI - 2, colI - 2, input) == 'A' && getCharAt(rowI - 3, colI - 3, input) == 'S' ? 1 : 0);
     }
 
-    static int countCrossMASAtPosition(int rowI, int columnI, char[][] input) {
-        return getCharAt(rowI, columnI, input) != 'A' ? 0 : (
-                  (getCharAt(rowI - 1, columnI - 1, input) == 'M' && getCharAt(rowI + 1, columnI + 1, input) == 'S' ? 1 : 0) +
-                  (getCharAt(rowI + 1, columnI + 1, input) == 'M' && getCharAt(rowI - 1, columnI - 1, input) == 'S' ? 1 : 0) +
-                  (getCharAt(rowI - 1, columnI + 1, input) == 'M' && getCharAt(rowI + 1, columnI - 1, input) == 'S' ? 1 : 0) +
-                  (getCharAt(rowI + 1, columnI - 1, input) == 'M' && getCharAt(rowI - 1, columnI + 1, input) == 'S' ? 1 : 0)
+    static int countCrossMASAtPosition(int rowI, int colI, char[][] input) {
+        return getCharAt(rowI, colI, input) != 'A' ? 0 : (
+                  (getCharAt(rowI - 1, colI - 1, input) == 'M' && getCharAt(rowI + 1, colI + 1, input) == 'S' ? 1 : 0) +
+                  (getCharAt(rowI + 1, colI + 1, input) == 'M' && getCharAt(rowI - 1, colI - 1, input) == 'S' ? 1 : 0) +
+                  (getCharAt(rowI - 1, colI + 1, input) == 'M' && getCharAt(rowI + 1, colI - 1, input) == 'S' ? 1 : 0) +
+                  (getCharAt(rowI + 1, colI - 1, input) == 'M' && getCharAt(rowI - 1, colI + 1, input) == 'S' ? 1 : 0)
                ) / 2;
     }
 
-    static char getCharAt(int rowI, int columnI, char[][] input) {
-        var rowCount = input.length;
-        var columnCount = input[0].length;
-
-        return rowI < 0 || columnI < 0 || rowI >= rowCount || columnI >= columnCount ? '0' : input[rowI][columnI];
+    static char getCharAt(int rowI, int colI, char[][] input) {
+        return rowI < 0 || colI < 0 || rowI >= input.length || colI >= input[0].length ? '0' : input[rowI][colI];
     }
 }
