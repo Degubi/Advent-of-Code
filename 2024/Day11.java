035beb758ce1e8117df87d0e16d1552d9e10a4d1
Degubi <gergonoorbi@gmail.com>
2024-12-11 15:17:57 +0100

    Upload Day 11 of 2024

---
+++ 2024/Day11.java
@@ -0,0 +1,54 @@
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+
+    record CacheEntry(long stone, long count) {}
+
+    public static void main(String[] args) throws Exception {
+        var inputStones = Arrays.stream(Files.readString(Path.of("input.txt")).trim().split(" "))
+                                .mapToLong(Long::parseLong)
+                                .toArray();
+
+        var stoneCountCache = new HashMap<CacheEntry, Long>();
+        var part1Result = Arrays.stream(inputStones)
+                                .map(k -> getStoneCount(k, 25, stoneCountCache))
+                                .sum();
+
+        var part2Result = Arrays.stream(inputStones)
+                                .map(k -> getStoneCount(k, 75, stoneCountCache))
+                                .sum();
+
+        System.out.println("Result 1: " + part1Result + ", result 2: " + part2Result);
+    }
+
+    static long getStoneCount(long stone, long count, HashMap<CacheEntry, Long> stoneCountCache) {
+        if(count == 0) return 1;
+
+        var cacheKey = new CacheEntry(stone, count);
+        var cached = stoneCountCache.get(cacheKey);
+
+        if(cached != null) return cached;
+
+        var nextCount = count - 1;
+        if(stone == 0) {
+            var result = getStoneCount(1, nextCount, stoneCountCache);
+            stoneCountCache.put(cacheKey, result);
+            return result;
+        }
+
+        var digitCount = ((int) Math.floor(Math.log10(stone))) + 1;
+        if(digitCount % 2 == 0) {
+             var idk = (long) Math.pow(10, digitCount / 2);
+             var result = getStoneCount(stone / idk, nextCount, stoneCountCache) + getStoneCount(stone % idk, nextCount, stoneCountCache);
+
+             stoneCountCache.put(cacheKey, result);
+             return result;
+        }
+
+        var result = getStoneCount(stone * 2024, nextCount, stoneCountCache);
+        stoneCountCache.put(cacheKey, result);
+        return result;
+    }
+}
