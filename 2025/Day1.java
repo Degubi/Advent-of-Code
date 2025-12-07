fd827a8b8c7c3e77d284e644f8aa4a7bc6119290
Degubi <gergonoorbi@gmail.com>
2025-12-07 17:43:56 +0100

    Upload 2025 Day1

---
+++ 2025/Day1.java
@@ -0,0 +1,33 @@
+static void main() throws Exception {
+    var instructions = Files.readAllLines(Path.of("input.txt"));
+
+    var states = Stream.iterate(new State(50, 0, 0), k -> k.instructionIndex == instructions.size() ? null : applyInstruction(k, instructions))
+                       .takeWhile(Objects::nonNull)
+                       .toArray(State[]::new);
+
+    var part1Result = Arrays.stream(states)
+                            .mapToInt(State::value)
+                            .filter(k -> k % 100 == 0)
+                            .count();
+
+    var part2Result = Arrays.stream(states)
+                            .mapToInt(State::rotations)
+                            .sum();
+
+    System.out.println("Result 1: " + part1Result + ", result 2: " + part2Result);
+}
+
+static State applyInstruction(State state, List<String> instructions) {
+    var instructionIndex = state.instructionIndex;
+    var instruction = instructions.get(instructionIndex);
+    var rotationCount = Integer.parseInt(instruction, 1, instruction.length(), 10);
+    var directionMultiplier = instruction.charAt(0) == 'L' ? -1 : 1;
+    var previousValue = state.value;
+    var nextValue = previousValue + rotationCount * directionMultiplier;
+    var rotations = directionMultiplier == 1 ? Math.floorDiv(nextValue, 100) - Math.floorDiv(previousValue, 100)
+                                             : Math.ceilDiv(previousValue, 100) - Math.ceilDiv(nextValue, 100);
+
+    return new State(nextValue, instructionIndex + 1, rotations);
+}
+
+record State(int value, int instructionIndex, int rotations) {}
