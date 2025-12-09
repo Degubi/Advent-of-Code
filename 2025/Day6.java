dd563b87d3c0117437e4e20c530a762884a8e2f6
Degubi <gergonoorbi@gmail.com>
2025-12-09 21:10:12 +0100

    Upload 2025 Day6 Part2

--- 2025/Day6.java
+++ 2025/Day6.java
@@ -1,46 +1,56 @@
 static void main() throws Exception {
-    var splitterRegex = Pattern.compile("\\s+");
-    var inputLines = Files.lines(Path.of("input.txt"))
-                          .map(String::trim)
-                          .map(splitterRegex::split)
-                          .toArray(String[][]::new);
+    var inputLines = Files.readAllLines(Path.of("input.txt"));
+    var valueStringLines = inputLines.subList(0, inputLines.size() - 1);
+    var operatorsLine = inputLines.getLast();
+    var operatorColumnIndices = IntStream.range(0, operatorsLine.length())
+                                         .filter(i -> operatorsLine.charAt(i) != ' ')
+                                         .toArray();
 
-    var operators = inputLines[inputLines.length - 1];
+    var columnValueStrings = IntStream.range(0, operatorColumnIndices.length)
+                                      .mapToObj(c -> valueStringLines.stream()
+                                                                     .map(k -> k.substring(operatorColumnIndices[c], c == operatorColumnIndices.length - 1 ? k.length() : operatorColumnIndices[c + 1]))
+                                                                     .toArray(String[]::new))
+                                      .toArray(String[][]::new);
 
-    var columnCount = inputLines[0].length;
-    var numbers = Arrays.stream(inputLines, 0, inputLines.length - 1)
-                        .map(k -> Arrays.stream(k).mapToInt(Integer::parseInt).toArray())
-                        .toArray(int[][]::new);
+    var operators = Arrays.stream(operatorColumnIndices)
+                          .mapToObj(operatorsLine::charAt)
+                          .toArray(Character[]::new);
 
+    var columnCount = operators.length;
 
     var part1Result = IntStream.range(0, columnCount)
-                               .mapToLong(c -> calculatePart1ColumnValue(c, numbers, operators[c].charAt(0)))
+                               .mapToLong(c -> calculateResult(producingPart1Values(columnValueStrings[c]), operators[c]))
                                .sum();
 
     var part2Result = IntStream.range(0, columnCount)
-                               .mapToLong(c -> calculatePart2ColumnValue(c, numbers, operators[c].charAt(0)))
+                               .mapToLong(c -> calculateResult(producingPart2Values(columnValueStrings[c]), operators[c]))
                                .sum();
 
     System.out.println("Result 1: " + part1Result + ", result 2: " + part2Result);
 }
 
-static long calculatePart1ColumnValue(int columnIndex, int[][] numbers, char operator) {
-    return IntStream.range(0, numbers.length)
-                    .mapToLong(i -> numbers[i][columnIndex])
-                    .reduce(operator == '+' ? 0 : 1, operator == '+' ? (k, l) -> k + l : (k, l) -> k * l);
+static long calculateResult(LongStream stream, char operator) {
+    return stream.reduce(operator == '+' ? 0 : 1, operator == '+' ? (k, l) -> k + l : (k, l) -> k * l);
 }
 
-static long calculatePart2ColumnValue(int columnIndex, int[][] numbers, char operator) {
-    var columnValues = IntStream.range(0, numbers.length)
-                                .mapToObj(i -> Integer.toString(numbers[i][columnIndex]))
-                                .toArray(String[]::new);
+static LongStream producingPart1Values(String[] valueStrings) {
+    return Arrays.stream(valueStrings)
+                 .map(String::trim)
+                 .mapToLong(Long::parseLong);
+}
 
-    var innerColumnCount = Arrays.stream(columnValues)
+static LongStream producingPart2Values(String[] valueStrings) {
+    var valueColumnCount = Arrays.stream(valueStrings)
                                  .mapToInt(String::length)
                                  .max()
                                  .orElseThrow();
 
-    // FIXME part2
-
-    return 0;
+    return IntStream.range(0, valueColumnCount)
+                    .mapToObj(valueColI -> Arrays.stream(valueStrings)
+                                                 .map(k -> k.charAt(valueColI))
+                                                 .filter(k -> k != ' ')
+                                                 .map(String::valueOf)
+                                                 .collect(Collectors.joining()))
+                    .filter(k -> !k.isEmpty())
+                    .mapToLong(Long::parseLong);
 }
