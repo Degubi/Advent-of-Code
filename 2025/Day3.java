f6b64c0a1d7e9543f9a05642ecab5290afa9b7b7
Degubi <gergonoorbi@gmail.com>
2025-12-28 15:01:42 +0100

    Complete 2025 Day3

--- 2025/Day3.java
+++ 2025/Day3.java
@@ -1,71 +1,33 @@
 static void main() throws Exception {
-    var banks = Files.readAllLines(Path.of("input.txt"));
+    var bankDigits = Files.lines(Path.of("input.txt"))
+                          .map(k -> k.chars().map(Character::getNumericValue).toArray())
+                          .toArray(int[][]::new);
 
-    var part1Result = banks.stream()
-                           .mapToLong(k -> findLargestJoltage(k, 2))
-                           .sum();
+    var part1Result = Arrays.stream(bankDigits)
+                            .mapToLong(k -> findMaxJoltage(k, 2))
+                            .sum();
 
-    // FIXME
-    var part2Result = banks.stream()
-                           .mapToLong(k -> findLargestJoltage(k, 12))
-                           .sum();
+    var part2Result = Arrays.stream(bankDigits)
+                            .mapToLong(k -> findMaxJoltage(k, 12))
+                            .sum();
 
     System.out.println("Result 1: " + part1Result + ", result 2: " + part2Result);
 }
 
-static long findLargestJoltage(String bank, int digitCount) {
-    var sortedDigits = IntStream.range(0, bank.length())
-                                .mapToObj(i -> new Digit(i, Character.getNumericValue(bank.charAt(i))))
-                                .sorted(Comparator.comparingInt(Digit::value).reversed())
-                                .toArray(Digit[]::new);
+static long findMaxJoltage(int[] digits, int resultDigitCount) {
+    var resultDigits = new ArrayList<Integer>();
+    var digitCountToRemove = digits.length - resultDigitCount;
 
-    return IntStream.range(0, sortedDigits.length)
-                    // .mapToObj(i -> digitCount == 12 ? part2(i, sortedDigits) : part1(i, sortedDigits))
-                    .mapToObj(i -> part1(i, sortedDigits))
-                    .flatMap(Optional::stream)
-                    .mapToLong(k -> sumDigits(k))
-                    .findFirst()
-                    .orElseThrow();
-}
-
-static Optional<Digit[]> part1(int i, Digit[] sortedDigits) {
-    return findNextDigit(sortedDigits[i], sortedDigits)
-          .map(k -> new Digit[] { sortedDigits[i], k });
-}
-
-static Optional<Digit[]> part2(int i, Digit[] sortedDigits) {
-    var digits = new Digit[12];
-    var previousDigit = sortedDigits[i];
-    var previousDigitStoreIndex = 0;
-    digits[0] = sortedDigits[i];
-
-    for(var l = 0; l < 11; ++l) {
-        var next = findNextDigit(previousDigit, sortedDigits);
-
-        if(next.isPresent()) {
-            var value = next.get();
-
-            digits[++previousDigitStoreIndex] = value;
-            previousDigit = value;
-        }else{
-            return Optional.empty();
+    for (var digit : digits) {
+        while (!resultDigits.isEmpty() && digitCountToRemove > 0 && resultDigits.getLast() < digit) {
+            resultDigits.removeLast();
+            --digitCountToRemove;
         }
+
+        resultDigits.add(digit);
     }
 
-    return Optional.of(digits);
-}
-
-static Optional<Digit> findNextDigit(Digit previousDigit, Digit[] sortedDigits) {
-    return IntStream.range(0, sortedDigits.length)
-                    .mapToObj(i -> sortedDigits[i])
-                    .filter(k -> k.bankIndex > previousDigit.bankIndex)
-                    .findFirst();
-}
-
-static long sumDigits(Digit[] digits) {
-    return IntStream.range(0, digits.length)
-                    .mapToLong(i -> digits[i].value * (long) Math.pow(10, digits.length - i - 1))
+    return IntStream.range(0, resultDigitCount)
+                    .mapToLong(i -> resultDigits.get(i) * (long) Math.pow(10, resultDigitCount - i - 1))
                     .sum();
 }
-
-record Digit(int bankIndex, int value) {}
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-<p>Total stars: <span style="color: #ffff66;">202*</span></p>
-<div><a href="2025">[2025]</a>&ensp;13*</div>
+<p>Total stars: <span style="color: #ffff66;">203*</span></p>
+<div><a href="2025">[2025]</a>&ensp;14*</div>
 <div><a href="2024">[2024]</a>&ensp;29*</div>
 <div><a href="2023">[2023]</a>&ensp;25*</div>
 <div><a href="2022">[2022]</a>&ensp;18*</div>
